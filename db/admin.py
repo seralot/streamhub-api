0e1aa3b216aee8a6531b52f3fa143c02fda024a3
revelio <revelio@localhost.localdomain>
2019-12-03 15:53:39 +0100

    improved models

--- db/admin.py
+++ db/admin.py
@@ -1,29 +1,81 @@
+from typing import Callable, Optional, TypeVar
+
 from django.contrib import admin
 
 from . import models
+
 # Register your models here.
 
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release', 'runtime', 'director', 'imdbRating', 'platform')
-    list_filter = ('platform',)
+ReturnType = TypeVar("ReturnType")
+FuncType = Callable[..., ReturnType]
+Func = TypeVar("Func", bound=FuncType)
 
+# Source https://github.com/escaped/django-admin-display
+def admin_display(
+    admin_order_field: Optional[str] = None,
+    boolean: Optional[bool] = None,
+    empty_value_display: Optional[str] = None,
+    short_description: Optional[str] = None,
+) -> Callable[[Func], Func]:
+    def wrapper(func: Func) -> Func:
+        if admin_order_field is not None:
+            setattr(func, "admin_order_field", admin_order_field)
+        if boolean is not None:
+            setattr(func, "boolean", boolean)
+        if empty_value_display is not None:
+            setattr(func, "empty_value_display", empty_value_display)
+        if short_description is not None:
+            setattr(func, "short_description", short_description)
+        return func
+
+    return wrapper
+
+
+@admin.register(models.Content)
 class ContentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'release', 'runtime', 'director', 'imdbRating', 'typeContent', 'platform')
-    list_filter = ('platform',)
+    list_display = (
+        "title",
+        "release",
+        "runtime",
+        "director",
+        'genre',
+        "imdbRating",
+        "typeContent",
+    )
+    list_filter = ("typeContent",)
 
-class ContentInline(admin.TabularInline):
-    model = models.Content
-    readonly_fields = ('title', 'release', 'runtime', 'director','imdbRating','typeContent')
-    exclude = ('episodes','seasons', 'description','poster')
-   
-class ContentPlatform(admin.ModelAdmin):
-    fieldsets = [
-        ('Plataforma',               {'fields': ['name', 'price']})
-    ]
-    inlines = [ContentInline]
+@admin.register(models.PlatformContent)
+class PlatformContentAdmin(admin.ModelAdmin):
+    list_display = ("platform", "title", "release", 'runtime', 'imdbRating', 'typeContent')
+    list_filter = ("platform",)
+
+    @admin_display(short_description="Titulo")
+    def title(self, obj):
+        return obj.content.title
 
 
-admin.site.register(models.Content, ContentAdmin)
-admin.site.register(models.Director)
-admin.site.register(models.Platform, ContentPlatform)
+    @admin_display(short_description="Estreno")
+    def release(self, obj):
+        return obj.content.release
+
+    @admin_display(short_description="Duración")
+    def runtime(self, obj):
+        return obj.content.runtime
+
+    @admin_display(short_description="Puntuación")
+    def imdbRating(self, obj):
+        return obj.content.imdbRating
+
+    @admin_display(short_description="Tipo")
+    def typeContent(self, obj):
+        return obj.content.typeContent
+
+
+@admin.register(models.Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pricePremium', 'priceStandard', 'priceBasic', 'content4k')
+
+@admin.register(models.Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname')
 
